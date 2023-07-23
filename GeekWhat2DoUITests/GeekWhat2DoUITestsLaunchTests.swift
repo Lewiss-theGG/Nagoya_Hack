069089cba99f05a5242f44fd6ec4483acc459810
Lewiss-theGG <gkin10130817@gmail.com>
2023-07-23 17:32:06 +0900

    Initial Commit

---
+++ GeekWhat2DoUITests/GeekWhat2DoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GeekWhat2DoUITestsLaunchTests.swift
+//  GeekWhat2DoUITests
+//
+//  Created by gkin on 2023/07/23.
+//
+
+import XCTest
+
+final class GeekWhat2DoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
