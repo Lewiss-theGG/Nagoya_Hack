de2e67d1c3f9ff07fc817f407bf1246002c902d7
Lewiss-theGG <gkin10130817@gmail.com>
2023-07-26 15:26:17 +0900

    openAI API implemented
    Json must be decoded

--- GeekWhat2Do/ChatViewController.swift
+++ GeekWhat2Do/ChatViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 
-class ChatViewController: UIViewController {
-    
-    let apiKey = "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"
-    
-    
-    let orgId = "org-LhxSkR9pTVukad8pbSLrdN5j"
+let apiKey = "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"
+
+
+let orgId = "org-LhxSkR9pTVukad8pbSLrdN5j"
+
+
+class ChatViewController: ViewWithAnimationViewController {
     
     
     let msgField = UITextField()
@@ -25,7 +26,7 @@ class ChatViewController: UIViewController {
     let responseView = UITextView()
     
     
-    var response = String()
+    private var response = String()
     
     
     override func viewDidLoad() {
@@ -40,7 +41,7 @@ class ChatViewController: UIViewController {
         msgField.backgroundColor = .secondarySystemBackground
         msgField.borderStyle = .bezel
         msgField.frame = CGRect(x: view.width/12, y: 150, width: view.width*5/6, height: 50)
-        msgField.text = "hello!"
+        msgField.text = "おはよう!"
         
         
         view.addSubview(requestButton)
@@ -48,7 +49,8 @@ class ChatViewController: UIViewController {
         requestButton.backgroundColor = .link
         requestButton.setTitle("request", for: .normal)
         requestButton.pressAction()
-        requestButton.addTarget(self, action: #selector(APIRequest), for: .touchUpInside)
+        //requestButton.addTarget(self, action: #selector(APIRequest), for: .touchUpInside)
+        requestButton.addTarget(self, action: #selector(tappedWithAsyncAwaitButton), for: .touchUpInside)
         
         
         view.addSubview(responseView)
@@ -63,8 +65,143 @@ class ChatViewController: UIViewController {
     }
     
     
-    @objc func APIRequest() {
-        
-        
+    @objc func APIRequest() async {
+                
+        await request {
+            
+            Task{
+                self.responseView.text = self.response
+            }
+        }
     }
+}
+
+
+
+extension ChatViewController{
+    
+    
+    private func request(completion: @escaping () -> Void) async {
+        
+        //URL
+        guard let url = URL(string: "https://api.openai.com/v1/chat/completions") else {
+            return //"URL error"
+        }
+        
+        
+        guard let content = msgField.text else{
+            return //String()
+        }
+        
+        
+        //URLRequestを作成
+        var req = URLRequest(url: url)
+        req.httpMethod = "POST"
+        req.allHTTPHeaderFields = ["Authorization" : "Bearer \(apiKey)"
+                                   ,"OpenAI-Organization": orgId
+                                   ,"Content-Type" : "application/json"]
+        req.httpBody = """
+    {
+    "model" : "gpt-3.5-turbo"
+    ,"messages": [{"role": "user", "content": "\(content)"}]
+    }
+    """.data(using: .utf8)
+        
+        //URLSessionでRequest
+        guard let (data, urlResponse) = try? await URLSession.shared.data(for: req) else {
+            return //"URLSession error"
+        }
+        
+        //ResponseをHTTPURLResponseにしてHTTPレスポンスヘッダを得る
+        guard let httpStatus = urlResponse as? HTTPURLResponse else {
+            return //"HTTPURLResponse error"
+        }
+        
+        //BodyをStringに、失敗したらレスポンスコードを返す
+        guard let _response = String(data: data, encoding: .utf8) else {
+            return// "\(httpStatus.statusCode)"
+        }
+        print(_response)
+        
+        
+        response = _response
+        
+        completion()
+        // return response
+    }
+    
+    
+    @objc private func tappedWithAsyncAwaitButton() {
+        
+        print("async/await")
+        Task {
+            let res = await task1()
+            responseView.text = res
+        }
+    }
+    
+    
+    private func task1() async -> String {
+        
+        //URL
+        guard let url = URL(string: "https://api.openai.com/v1/chat/completions") else {
+            return "URL error"
+        }
+        
+        
+        guard let content = msgField.text else{
+            return String()
+        }
+        
+        
+        //URLRequestを作成
+        var req = URLRequest(url: url)
+        req.httpMethod = "POST"
+        req.allHTTPHeaderFields = ["Authorization" : "Bearer \(apiKey)"
+                                   ,"OpenAI-Organization": orgId
+                                   ,"Content-Type" : "application/json"]
+        req.httpBody = """
+    {
+    "model" : "gpt-3.5-turbo"
+    ,"messages": [{"role": "user", "content": "\(content)"}]
+    }
+    """.data(using: .utf8)
+        
+        //URLSessionでRequest
+        guard let (data, urlResponse) = try? await URLSession.shared.data(for: req) else {
+            return "URLSession error"
+        }
+        
+        //ResponseをHTTPURLResponseにしてHTTPレスポンスヘッダを得る
+        guard let httpStatus = urlResponse as? HTTPURLResponse else {
+            return "HTTPURLResponse error"
+        }
+        
+        //BodyをStringに、失敗したらレスポンスコードを返す
+        guard let _response = String(data: data, encoding: .utf8) else {
+            return "\(httpStatus.statusCode)"
+        }
+        print(_response)
+        
+        
+        //response = _response
+        
+        return _response
+    }
+}
+
+class ServerTaskWithAsyncAwait {
+
+
+//    private func task2() async -> Int {
+//        sleep(2)
+//        print("タスク2完了")
+//        return 1
+//    }
+//
+//    func executeBothTask() async -> Int {
+//        let num1 = await task1()
+//        let num2 = await task2()
+//        return num1 + num2
+//    }
 }
--- GeekWhat2Do/SceneDelegate.swift
+++ GeekWhat2Do/SceneDelegate.swift
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             if let windowScene = scene as? UIWindowScene {
                 
                 let window = UIWindow(windowScene: windowScene)
-                window.rootViewController = AuthViewController.init()
+                window.rootViewController = ChatViewController.init()
                 self.window = window
                 window.makeKeyAndVisible()
             }
