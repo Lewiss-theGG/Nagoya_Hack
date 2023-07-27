53a89c6efa956e4450bf1a9efe0e8eb63102fad1
Lewiss-theGG <gkin10130817@gmail.com>
2023-07-27 19:46:50 +0900

    20230727_Branch1#1

--- GeekWhat2Do/ChatViewController.swift
+++ GeekWhat2Do/ChatViewController.swift
@@ -29,6 +29,11 @@ class ChatViewController: ViewWithAnimationViewController {
     private var response = String()
     
     
+    let message = ChatGPTMassage(role: "user", content: content)
+    let request = ChatGPTRequest(model: "gpt-3.5-turbo", messages: [message])
+    let requestData try? JSONEncoder().encode(req)
+    
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -41,7 +46,7 @@ class ChatViewController: ViewWithAnimationViewController {
         msgField.backgroundColor = .secondarySystemBackground
         msgField.borderStyle = .bezel
         msgField.frame = CGRect(x: view.width/12, y: 150, width: view.width*5/6, height: 50)
-        msgField.text = "おはよう!"
+        msgField.text = response
         
         
         view.addSubview(requestButton)
@@ -49,8 +54,7 @@ class ChatViewController: ViewWithAnimationViewController {
         requestButton.backgroundColor = .link
         requestButton.setTitle("request", for: .normal)
         requestButton.pressAction()
-        //requestButton.addTarget(self, action: #selector(APIRequest), for: .touchUpInside)
-        requestButton.addTarget(self, action: #selector(tappedWithAsyncAwaitButton), for: .touchUpInside)
+        requestButton.addTarget(self, action: #selector(APIRequest), for: .touchUpInside)
         
         
         view.addSubview(responseView)
@@ -65,13 +69,11 @@ class ChatViewController: ViewWithAnimationViewController {
     }
     
     
-    @objc func APIRequest() async {
-                
-        await request {
+    @objc private func APIRequest() {
+        
+        Task {
             
-            Task{
-                self.responseView.text = self.response
-            }
+            responseView.text = await task1()
         }
     }
 }
@@ -79,67 +81,7 @@ class ChatViewController: ViewWithAnimationViewController {
 
 
 extension ChatViewController{
-    
-    
-    private func request(completion: @escaping () -> Void) async {
-        
-        //URL
-        guard let url = URL(string: "https://api.openai.com/v1/chat/completions") else {
-            return //"URL error"
-        }
-        
-        
-        guard let content = msgField.text else{
-            return //String()
-        }
-        
-        
-        //URLRequestを作成
-        var req = URLRequest(url: url)
-        req.httpMethod = "POST"
-        req.allHTTPHeaderFields = ["Authorization" : "Bearer \(apiKey)"
-                                   ,"OpenAI-Organization": orgId
-                                   ,"Content-Type" : "application/json"]
-        req.httpBody = """
-    {
-    "model" : "gpt-3.5-turbo"
-    ,"messages": [{"role": "user", "content": "\(content)"}]
-    }
-    """.data(using: .utf8)
-        
-        //URLSessionでRequest
-        guard let (data, urlResponse) = try? await URLSession.shared.data(for: req) else {
-            return //"URLSession error"
-        }
-        
-        //ResponseをHTTPURLResponseにしてHTTPレスポンスヘッダを得る
-        guard let httpStatus = urlResponse as? HTTPURLResponse else {
-            return //"HTTPURLResponse error"
-        }
-        
-        //BodyをStringに、失敗したらレスポンスコードを返す
-        guard let _response = String(data: data, encoding: .utf8) else {
-            return// "\(httpStatus.statusCode)"
-        }
-        print(_response)
-        
-        
-        response = _response
-        
-        completion()
-        // return response
-    }
-    
-    
-    @objc private func tappedWithAsyncAwaitButton() {
-        
-        print("async/await")
-        Task {
-            let res = await task1()
-            responseView.text = res
-        }
-    }
-    
+
     
     private func task1() async -> String {
         
@@ -190,18 +132,14 @@ extension ChatViewController{
     }
 }
 
-class ServerTaskWithAsyncAwait {
+
+struct ChatGPTRequest:Codable{
+    let model:String
+    let messages:[ChatGPTMassage]
+}
 
 
-//    private func task2() async -> Int {
-//        sleep(2)
-//        print("タスク2完了")
-//        return 1
-//    }
-//
-//    func executeBothTask() async -> Int {
-//        let num1 = await task1()
-//        let num2 = await task2()
-//        return num1 + num2
-//    }
+struct ChatGPTMassage:Codable{
+    let role:String
+    let content:String
 }
